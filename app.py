d6d4bf616bedea3fc3de969bc0804ffb76e179f1
takeuchisatoshi <satsitt3@gmail.com>
2019-03-12 11:21:24 +0900

    おみくじ追加

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import random
+
+
 from flask import Flask
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_world():
@@ -12,14 +14,22 @@ def hello_world():
 def hello_hachimantai():
     return "<html>スパルタキャンプ in 八幡平</html>"
 
+
 @app.route("/goodbye")
 def goodbye():
     return "<html>Good Bye!</html>"
 
 
+@app.route("/omikuji")
+def omikuji():
+    fortune_list = ["大吉", "中吉", "小吉", "凶", "大凶"]
+    fortune = random.choice(fortune_list)
+
+    return f"今日の運勢は...{fortune}です！"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
 
 """
 課題
